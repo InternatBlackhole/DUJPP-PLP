d8d93ef7de965599fdf8f236cd904167f1c94127
InternatBlackhole <timmy@timkr.si>
2025-09-25 02:56:21 +0200

    Konfiguracija razvojnega okolja

--- PLPWebPage/vite.config.ts
+++ PLPWebPage/vite.config.ts
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, HmrOptions, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
+const confObj: UserConfig = {
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,4 +14,26 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+  server: {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+      exposedHeaders: ['Content-Range', 'X-Content-Range'],
+      credentials: true,
+      maxAge: 3600
+    },
+  }
+}
+
+if(process.env.VITE_HOST) {
+  const [host, port] = process.env.VITE_HOST.split(/^https?:\/\/(.+):([0-9]{1,5})$/, 2)
+  confObj.server!.hmr = {
+    protocol: "ws",
+    host,
+    port: Number.parseInt(port)
+  } satisfies HmrOptions
+}
+
+// https://vite.dev/config/
+export default defineConfig(confObj)
